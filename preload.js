c6e74d592a192fce7ebdd52c62ce4b10ae433374
Jesse <jesse.greenberg@colorado.edu>
2021-11-22 15:50:55 -0500

    formatting and a .gitignore

--- preload.js
+++ preload.js
@@ -1,35 +1,36 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-      const element = document.getElementById(selector)
-      if (element) element.innerText = text
+window.addEventListener( 'DOMContentLoaded', () => {
+  const replaceText = ( selector, text ) => {
+    const element = document.getElementById( selector )
+    if ( element ) {
+      element.innerText = text
     }
-    const io = require('socket.io-client');
-    var socket = io();
+  }
+  const io = require( 'socket.io-client' );
+  var socket = io();
 
-    
-window.addEventListener( 'message', event => {
+
+  window.addEventListener( 'message', event => {
     if ( !event.data ) {
-    return;
+      return;
     }
-    
+
     let data;
     try {
-    data = JSON.parse( event.data );
+      data = JSON.parse( event.data );
     }
     catch( e ) {
-    return;
+      return;
     }
     //debugger
     // if load is successful, create a visualization of the parallel DOM
     if ( data.type === 'load' ) {
-    
-    
 
-        const simFrameWindow = document.getElementById("iframe").contentWindow;
-        if (simFrameWindow.phet) {
+
+      const simFrameWindow = document.getElementById( "iframe" ).contentWindow;
+      if ( simFrameWindow.phet ) {
 
         const simModel = simFrameWindow.simModel;
-        console.log(simFrameWindow.phet);
+        console.log( simFrameWindow.phet );
         window.simulationWindow = simFrameWindow;
         /*
         const vertex1 = simFrameWindow.vertex1;
@@ -40,61 +41,60 @@ window.addEventListener( 'message', event => {
         //var positionProperty = simFrameWindow.positionProperty
         //debugger
         var dot = simFrameWindow.phet.dot
-        const physicalToSimLengthMap = new dot.LinearFunction(5.5, 9.2, 0.05, 1.0);
-    
+        const physicalToSimLengthMap = new dot.LinearFunction( 5.5, 9.2, 0.05, 1.0 );
+
         var socket = io();
         // we are ready to start calibrating, start populating values
-        
-    
-        socket.on('data', function(data) {
-        pdata = JSON.parse(data);
-        var angle1 = pdata.angle1;
-        var angle2 = pdata.angle2;
-        var angle3 = pdata.angle3;
-        var angle4 = pdata.angle4;
-    
-        var lengthA = pdata.lengthA;
-        var lengthB = pdata.lengthB;
-        var lengthC = pdata.lengthC;
-        var lengthD = pdata.lengthD;
-        
-        if (simModel.isCalibratingProperty.value){
-            simModel.setPhysicalModelBounds( lengthC,lengthB,lengthA,lengthD);
-        }
-        console.log(angle1);
-        console.log(angle2);
-        console.log(angle3);
-        console.log(angle4);
-        console.log("");
-        console.log("lengthA:",lengthA);
-        console.log("lengthB:",lengthB);
-        console.log("lengthC:",lengthC);
-        console.log("lengthD:",lengthD);
-        console.log("");
-        simModel.setPositionsFromLengthAndAngleData(lengthC,lengthB,lengthA,lengthD,angle1,angle4,angle2,angle3);
-    
-        /*
-        const newPositionX = physicalToSimLengthMap(lengthA);
-        const constrainedPosition = vertex3.positionProperty.validBounds.closestPointTo(new dot.Vector2(newPositionX,vertex3.positionProperty.value.y));
-        vertex3.positionProperty.value = constrainedPosition;
-        console.log(constrainedPosition);
-        //debugger
-        document.getElementById('sample').style.opacity = data+"%"; 
-        //positionProperty.set(positionProperty.get().plusXY(5,0))
-        */
-        });
-    
-        socket.emit("message", "hello");
-    }}
-    
-    })
 
-    
-    
-    
-    for (const dependency of ['chrome', 'node', 'electron']) {
-      replaceText(`${dependency}-version`, process.versions[dependency])
+
+        socket.on( 'data', function( data ) {
+          pdata = JSON.parse( data );
+          var angle1 = pdata.angle1;
+          var angle2 = pdata.angle2;
+          var angle3 = pdata.angle3;
+          var angle4 = pdata.angle4;
+
+          var lengthA = pdata.lengthA;
+          var lengthB = pdata.lengthB;
+          var lengthC = pdata.lengthC;
+          var lengthD = pdata.lengthD;
+
+          if ( simModel.isCalibratingProperty.value ) {
+            simModel.setPhysicalModelBounds( lengthC, lengthB, lengthA, lengthD );
+          }
+          console.log( angle1 );
+          console.log( angle2 );
+          console.log( angle3 );
+          console.log( angle4 );
+          console.log( "" );
+          console.log( "lengthA:", lengthA );
+          console.log( "lengthB:", lengthB );
+          console.log( "lengthC:", lengthC );
+          console.log( "lengthD:", lengthD );
+          console.log( "" );
+          simModel.setPositionsFromLengthAndAngleData( lengthC, lengthB, lengthA, lengthD, angle1, angle4, angle2, angle3 );
+
+          /*
+          const newPositionX = physicalToSimLengthMap(lengthA);
+          const constrainedPosition = vertex3.positionProperty.validBounds.closestPointTo(new dot.Vector2(newPositionX,vertex3.positionProperty.value.y));
+          vertex3.positionProperty.value = constrainedPosition;
+          console.log(constrainedPosition);
+          //debugger
+          document.getElementById('sample').style.opacity = data+"%";
+          //positionProperty.set(positionProperty.get().plusXY(5,0))
+          */
+        } );
+
+        socket.emit( "message", "hello" );
+      }
     }
-  })
+
+  } )
+
+
+  for ( const dependency of [ 'chrome', 'node', 'electron' ] ) {
+    replaceText( `${dependency}-version`, process.versions[ dependency ] )
+  }
+} )
 
 
